66cbeb8614587d0713bcdb0a7924034cd15774c6
Chris Malley <cmalley@pixelzoom.com>
2016-10-04 15:30:29 -0600

    cleanup

--- js/common/shopping/ShoppingConstants.js
+++ js/common/shopping/ShoppingConstants.js
@@ -13,18 +13,8 @@ define( function( require ) {
   var unitRates = require( 'UNIT_RATES/unitRates' );
 
   var ShoppingConstants = {
-
-    //----------------------------------------------------------------------------
-    // Views
-    //----------------------------------------------------------------------------
     DEFAULT_CORRECT_PROMPT_COLOR: 'rgba(0,0,225,1)',    // Challenge question color
     UNIT_RATE_CORRECT_PROMPT_COLOR: 'rgba(0,192,0,1)'     // Challenge Unit Rate question color
-
-    //----------------------------------------------------------------------------
-    // Models
-    //----------------------------------------------------------------------------
-
-
   };
 
   unitRates.register( 'ShoppingConstants', ShoppingConstants );
