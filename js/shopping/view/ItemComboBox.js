0c7483ab37fef6fdb6670bcf486436460330b3c2
Chris Malley <cmalley@pixelzoom.com>
2016-10-23 16:24:48 -0600

    #85 uniform icon sizes for items in combo box

--- js/shopping/view/ItemComboBox.js
+++ js/shopping/view/ItemComboBox.js
@@ -10,13 +10,17 @@ define( function( require ) {
 
   // modules
   var ComboBox = require( 'SUN/ComboBox' );
+  var Dimension2 = require( 'DOT/Dimension2' );
   var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Item = require( 'UNIT_RATES/common/shopping/model/Item' );
   var ItemNodeFactory = require( 'UNIT_RATES/common/shopping/view/ItemNodeFactory' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Text = require( 'SCENERY/nodes/Text' );
   var unitRates = require( 'UNIT_RATES/unitRates' );
+  var VStrut = require( 'SCENERY/nodes/VStrut' );
 
   /**
    * @param {ItemData[]} itemDataArray
@@ -28,18 +32,24 @@ define( function( require ) {
   function ItemComboBox( itemDataArray, itemDataProperty, parentNode, options ) {
 
     options = _.extend( {
+
+      // ItemComboBox options
+      iconSize: new Dimension2( 30, 30 ),
+
+      // ComboBox options
       listPosition: 'above',
       buttonCornerRadius: 5,
       listCornerRadius: 5,
-      itemYMargin: 0,
-      itemXMargin: 3,
-      maxWidth: 200
+      itemXMargin: 0,
+      itemYMargin: 5,
+      maxWidth: 220
+
     }, options );
 
     // describe items in the combo box
     var contentArray = [];
     itemDataArray.forEach( function( itemDatum ) {
-      contentArray.push( createItem( itemDatum ) );
+      contentArray.push( createItem( itemDatum, options.iconSize ) );
     } );
 
     ComboBox.call( this, contentArray, itemDataProperty, parentNode, options );
@@ -51,9 +61,10 @@ define( function( require ) {
    * Creates an item for the combo box.
    *
    * @param {ItemData} itemData
+   * @param {Dimension2} iconSize
    * @returns {{node: Node, value: *}}
    */
-  function createItem( itemData ) {
+  function createItem( itemData, iconSize ) {
 
     //TODO because candies don't have pluralName, this is wonky
     var labelString = itemData.pluralName ?  itemData.pluralName : itemData.singularName;
@@ -62,7 +73,20 @@ define( function( require ) {
       font: new PhetFont( 18 ),
       maxWidth: 140
     } );
-    var itemIcon = ItemNodeFactory.createItemNode( new Item( itemData.type, 1 ) );
+
+    var iconNode = ItemNodeFactory.createItemNode( new Item( itemData.type, 1 ) );
+    if ( iconNode.width > iconSize.width || iconNode.height > iconSize.height ) {
+      var scale = Math.min(  iconSize.width / iconNode.width, iconSize.height / iconNode.height );
+      iconNode.setScaleMagnitude( scale );
+    }
+
+    var itemIcon = new Node( {
+      children: [
+        iconNode,
+        new HStrut( iconSize.width, { center: iconNode.center } ),
+        new VStrut( iconSize.height, { center: iconNode.center } )
+      ]
+    } );
 
     var itemNode = new HBox( {
       spacing: 10,
