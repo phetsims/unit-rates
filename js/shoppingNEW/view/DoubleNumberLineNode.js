bc7541568e385b38be3756db3cd54bbef0cc60da
Chris Malley <cmalley@pixelzoom.com>
2016-10-25 10:10:23 -0600

    cleanup

--- js/shoppingNEW/view/DoubleNumberLineNode.js
+++ js/shoppingNEW/view/DoubleNumberLineNode.js
@@ -11,6 +11,7 @@ define( function( require ) {
 
   // modules
   var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
+  var Dimension2 = require( 'DOT/Dimension2' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -24,52 +25,68 @@ define( function( require ) {
 
     options = _.extend( {
 
-      length: 725, // length of both lines
-      spacing: 50, // vertical spacing between top and bottom line
-
-      // top line
-      topLineColor: 'black',
-      topLineWidth: 1,
-      topLabel: '',
-      topLabelColor: 'black',
-
-      // bottom line
-      bottomLineColor: 'black',
-      bottomLineWidth: 1,
-      bottomLabel: '',
-      bottomLabelColor: 'black',
-
-      // vertical line
-      verticalLineExtension: 30,
-      verticalLineColor: 'black',
-      verticalLineWidth: 1
+      // vertical axis
+      verticalAxisLength: 60,
+      verticalAxisColor: 'black',
+      verticalAxisLineWidth: 1.5,
+      
+      // common to both horizontal axes
+      horizontalAxisLength: 725,
+      horizontalAxisLineWidth: 1.5,
+      arrowSize: new Dimension2( 8, 8 ),
+      horizontalAxisYSpacing: 20, // vertical spacing between top and bottom axes
+      labelXSpacing: 2, // horizontal spacing between axis and its label
+      
+      // top horizontal axis
+      topLabel: null, // {Node|null} label on the top axis
+      topAxisColor: 'black',
+      
+      // bottom horizontal axis
+      bottomLabel: null, // {Node|null} label on the bottom axis
+      bottomAxisColor: 'black'
 
     }, options );
 
     Node.call( this, options );
 
-    var verticalLine = new Line( 0, 0, 0, options.spacing + ( 2 * options.verticalLineExtension ), {
-      stroke: options.verticalLineColor,
-      lineWidth: options.verticleLineWidth
+    var verticalAxis = new Line( 0, 0, 0, options.verticalAxisLength, {
+      stroke: options.verticalAxisColor,
+      lineWidth: options.verticalAxisLineWidth
     } );
-    this.addChild( verticalLine );
+    this.addChild( verticalAxis );
 
-    var topLineNode = new ArrowNode( 0, 0, options.length, 0, {
-      fill: options.topLineColor,
+    var topAxisNode = new ArrowNode( 0, 0, options.horizontalAxisLength, 0, {
+      fill: options.topAxisColor,
       stroke: null,
-      tailWidth: options.topLineWidth,
-      x: verticalLine.x,
-      y: verticalLine.y + options.verticalLineExtension
+      headWidth: options.arrowSize.width,
+      headHeight: options.arrowSize.height,
+      tailWidth: options.horizontalAxisLineWidth,
+      x: verticalAxis.x,
+      y: verticalAxis.centerY - ( options.horizontalAxisYSpacing / 2 )
     } );
-    this.addChild( topLineNode );
+    this.addChild( topAxisNode );
+    
+    if ( options.topLabel ) {
+      options.topLabel.left = topAxisNode.right + options.labelXSpacing;
+      options.topLabel.centerY =  topAxisNode.centerY;
+      this.addChild( options.topLabel );
+    }
 
-    var bottomLineNode = new ArrowNode( 0, 0, options.length, 0, {
-      fill: options.bottomLineColor,
+    var bottomAxisNode = new ArrowNode( 0, 0, options.horizontalAxisLength, 0, {
+      fill: options.bottomAxisColor,
       stroke: null,
-      tailWidth: options.bottomLineWidth,
-      y: topLineNode.y + options.spacing
+      headWidth: options.arrowSize.width,
+      headHeight: options.arrowSize.height,
+      tailWidth: options.horizontalAxisLineWidth,
+      y: verticalAxis.centerY + ( options.horizontalAxisYSpacing / 2 )
     } );
-    this.addChild( bottomLineNode );
+    this.addChild( bottomAxisNode );
+
+    if ( options.bottomLabel ) {
+      options.bottomLabel.left = bottomAxisNode.right + options.labelXSpacing;
+      options.bottomLabel.centerY =  bottomAxisNode.centerY;
+      this.addChild( options.bottomLabel );
+    }
 
     this.mutate( options );
   }
