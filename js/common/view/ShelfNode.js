1aa57c0cf0e9be7057cdef25f856173ab46f5848
Chris Malley <cmalley@pixelzoom.com>
2017-01-17 12:44:49 -0700

    draw shelf programmatically, #138

--- js/common/view/ShelfNode.js
+++ js/common/view/ShelfNode.js
@@ -9,15 +9,14 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Shape = require( 'KITE/Shape' );
 
   // sim modules
   var unitRates = require( 'UNIT_RATES/unitRates' );
-
-  // images
-  var shelfImage = require( 'image!UNIT_RATES/shelf.png' );
 
   /**
    * @param {Shelf} shelf
@@ -26,10 +25,33 @@ define( function( require ) {
    */
   function ShelfNode( shelf, options ) {
 
-    var shelfImageNode = new Image( shelfImage, { scale: 0.5 } );
+    options = _.extend( {
+      size: { width: 356, height: 17, depth: 20 },
+      perspectiveXOffset: 25,
+      fill: 'rgb( 174, 129, 91 )',
+      stroke: 'black'
+    }, options );
+
+    // front face
+    var frontNode = new Rectangle( 0, 0, options.size.width, options.size.height, {
+      fill: options.fill,
+      stroke: options.stroke
+    } );
+
+    // top face
+    var topShape = new Shape()
+      .moveTo( 0, 0 )
+      .lineTo( options.perspectiveXOffset, -options.size.depth )
+      .lineTo( options.size.width - options.perspectiveXOffset, -options.size.depth )
+      .lineTo( options.size.width, 0 )
+      .close();
+    var topNode = new Path( topShape, {
+      fill: options.fill,
+      stroke: options.stroke
+    } );
 
     assert && assert( !options.children, 'decoration not supported' );
-    options.children = [ shelfImageNode ];
+    options.children = [ frontNode, topNode ];
 
     Node.call( this, options );
   }
