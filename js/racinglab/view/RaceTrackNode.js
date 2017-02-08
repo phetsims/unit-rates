d644021d531558c77235f64f7de1b98316705486
Chris Malley <cmalley@pixelzoom.com>
2017-02-08 13:34:28 -0700

    add label to indicate length of race track

--- js/racinglab/view/RaceTrackNode.js
+++ js/racinglab/view/RaceTrackNode.js
@@ -16,18 +16,26 @@ define( function( require ) {
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  var Text = require( 'SCENERY/nodes/Text' );
 
   // sim modules
   var RaceTimerNode = require( 'UNIT_RATES/racinglab/view/RaceTimerNode' );
   var unitRates = require( 'UNIT_RATES/unitRates' );
+  var URFont = require( 'UNIT_RATES/common/URFont' );
 
   // images
   var finishFlagImage = require( 'image!UNIT_RATES/finish_flag.png' );
   var startFlagImage = require( 'image!UNIT_RATES/start_flag.png' );
 
+  // strings
+  var milesString = require( 'string!UNIT_RATES/miles' );
+  var valueUnitsString = require( 'string!UNIT_RATES/valueUnits' );
+
   // constants
   var NEGATIVE_TRACK_LENGTH = 75; // length of track to left of starting flag, in view coordinates
-
+  var MARKER_SIDE_LENGTH = 10;
+  
   /**
    * @param {RaceTrack} track - the track model
    * @param {LinearFunction} modelToView
@@ -67,6 +75,7 @@ define( function( require ) {
       // next marker
       x = x + track.markerSpacing;
     }
+    markersParent.top = solidLineNode.centerY;
 
     // Flag at starting line
     var startFlagNode = new Image( startFlagImage, {
@@ -82,24 +91,41 @@ define( function( require ) {
       bottom: 0
     } );
 
-    // Timer, above finish flag
-    var timerNode = new RaceTimerNode( {
-      centerX: finishFlagNode.left,
-      bottom: finishFlagNode.top - 3
-    } );
+    // Timer
+    var timerNode = new RaceTimerNode();
 
-    //TODO label track length, e.g. '200 miles'
+    // Label that indicates the length of the track
+    var lengthNode = new Text( '', { font: new URFont( 12 ) } );
+
     //TODO car
+    //TODO finish flag drag handler
 
     assert && assert( !options.children, 'decoration not supported' );
-    options.children = [ dashedLineNode, solidLineNode, markersParent, startFlagNode, finishFlagNode, timerNode ];
+    options.children = [ dashedLineNode, solidLineNode, markersParent,
+      startFlagNode, finishFlagNode, timerNode, lengthNode ];
 
     Node.call( this, options );
 
     // Synchronize track length with the model
     var lengthObserver = function( length ) {
+
       var finishX = modelToView( length );
+
+      // adjust track length
       solidLineNode.setLine( -NEGATIVE_TRACK_LENGTH, 0, finishX, 0 );
+
+      // flag at finish line
+      finishFlagNode.left = finishX;
+      finishFlagNode.bottom = solidLineNode.centerY;
+
+      // timer above finish flag
+      timerNode.centerX = finishX;
+      timerNode.bottom = finishFlagNode.top - 3;
+
+      // distance label below finish flag
+      lengthNode.text = StringUtils.format( valueUnitsString, finishX, milesString );
+      lengthNode.top = solidLineNode.bottom + MARKER_SIDE_LENGTH + 4;
+      lengthNode.centerX = finishX;
     };
     track.lengthProperty.link( lengthObserver ); // unlink in dispose
 
@@ -112,17 +138,15 @@ define( function( require ) {
   unitRates.register( 'RaceTrackNode', RaceTrackNode );
 
   /**
-   * Creates a track marker.
+   * Creates a track marker, an equilateral triangle, with origin at tip.
    * @returns {Node}
    */
   function createMarkerNode() {
     
-    // equilateral triangle, with origin at tip
-    var SIDE_LENGTH = 10;
     var markerShape = new Shape()
       .moveTo( 0, 0 )
-      .lineTo( SIDE_LENGTH / 2, SIDE_LENGTH )
-      .lineTo( -SIDE_LENGTH / 2, SIDE_LENGTH )
+      .lineTo( MARKER_SIDE_LENGTH / 2, MARKER_SIDE_LENGTH )
+      .lineTo( -MARKER_SIDE_LENGTH / 2, MARKER_SIDE_LENGTH )
       .close();
     
     return new Path( markerShape, {
