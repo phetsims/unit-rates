312548dca4e9f93522bd136035de4c7db49f072e
Chris Malley <cmalley@pixelzoom.com>
2017-03-06 13:41:12 -0700

    delete unused var

--- js/shopping/view/BaseShoppingSceneNode.js
+++ js/shopping/view/BaseShoppingSceneNode.js
@@ -96,8 +96,8 @@ define( function( require ) {
 
           // Create the item's Node. Adds itself to the proper layer, so there is no addChild call here,
           // and we need to disable eslint error with this comment line:
-          // eslint-disable-next-line no-unused-vars
-          var itemNode = new ShoppingItemNode( item, shoppingScene.shelf, shoppingScene.scale,
+          // eslint-disable-next-line no-new
+          new ShoppingItemNode( item, shoppingScene.shelf, shoppingScene.scale,
             frontItemLayer, backItemLayer, dragLayer );
         } );
       }
