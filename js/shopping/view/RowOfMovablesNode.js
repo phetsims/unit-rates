6097e8924372b4edf183e6d213dab83e371e24dd
phet-dev <phetadmin@gmail.com>
2020-01-08 03:31:41 -0700

    update copyright dates from daily grunt work

--- js/shopping/view/RowOfMovablesNode.js
+++ js/shopping/view/RowOfMovablesNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Used for debugging purposes to see the cells in each RowOfMovable instance.
