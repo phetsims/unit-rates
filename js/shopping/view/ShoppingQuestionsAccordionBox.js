3b803cdaf63d8b6630771576465f7c7b5bd80ccd
pixelzoom <cmalley@pixelzoom.com>
2020-03-10 13:43:04 -0600

    convert to class, https://github.com/phetsims/tasks/issues/1026

--- js/shopping/view/ShoppingQuestionsAccordionBox.js
+++ js/shopping/view/ShoppingQuestionsAccordionBox.js
@@ -7,7 +7,6 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import RefreshButton from '../../../../scenery-phet/js/buttons/RefreshButton.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
@@ -22,125 +21,125 @@ import ShoppingQuestionNode from './ShoppingQuestionNode.js';
 
 const questionsString = unitRatesStrings.questions;
 
-/**
- * @param {ShoppingScene} shoppingScene
- * @param {Node} keypadLayer
- * @param {Object} [options]
- * @constructor
- */
-function ShoppingQuestionsAccordionBox( shoppingScene, keypadLayer, options ) {
+class ShoppingQuestionsAccordionBox extends AccordionBox {
+  /**
+   * @param {ShoppingScene} shoppingScene
+   * @param {Node} keypadLayer
+   * @param {Object} [options]
+   */
+  constructor( shoppingScene, keypadLayer, options ) {
 
-  options = merge( {}, URConstants.ACCORDION_BOX_OPTIONS, {
+    options = merge( {}, URConstants.ACCORDION_BOX_OPTIONS, {
 
-    // AccordionBox options
-    // tight vertical margins and spacing, see https://github.com/phetsims/unit-rates/issues/140
-    titleYMargin: 0,
-    contentXMargin: 10,
-    contentYMargin: 6,
-    contentYSpacing: 2,
-    titleNode: new Text( questionsString, {
-      font: URConstants.ACCORDION_BOX_TITLE_FONT,
-      maxWidth: 100
-    } ),
-
-    // VBox options
-    vBoxSpacing: 12 // vertical spacing between UI elements in the accordion box's content
-
-  }, options );
-
-  // An invisible rectangle that has the same bounds as the accordion box. Used to position the keypad.
-  // Dimensions will be set after calling super.  This was added so when converting to an ES6 class, because
-  // we can't use this before super.  See https://github.com/phetsims/tasks/issues/1026#issuecomment-594357784
-  const thisBoundsNode = new Rectangle( 0, 0, 1, 1, {
-    visible: false,
-    pickable: false
-  } );
-
-  // 'Unit Rate?' question, dispose required.
-  // This question is separate because it does not change when the refresh button is pressed.
-  const unitRateQuestionNode = new ShoppingQuestionNode( shoppingScene.unitRateQuestion, thisBoundsNode, keypadLayer, {
-    denominatorVisible: true
-  } );
-
-  // Below the 'Unit Rate?' question is a set of questions that change when the refresh button is pressed.
-  const questionsParent = new VBox( {
-    align: 'right',
-    spacing: options.vBoxSpacing
-  } );
-  const questionSetObserver = questionSet => {
-
-    // remove previous questions
-    questionsParent.getChildren().forEach( child => {
-      assert && assert( child instanceof ShoppingQuestionNode );
-      child.dispose();
-    } );
-    questionsParent.removeAllChildren();
-
-    // add new questions, dispose required
-    const questionNodes = [];
-    for ( let i = 0; i < questionSet.length; i++ ) {
-      questionNodes.push( new ShoppingQuestionNode( questionSet[ i ], thisBoundsNode, keypadLayer ) );
-    }
-    questionsParent.setChildren( questionNodes );
-  };
-  shoppingScene.questionSetProperty.link( questionSetObserver ); // unlink in dispose
-
-  // Refresh button, advances to the next question set
-  const refreshButton = new RefreshButton( {
-    iconScale: 0.36,
-    xMargin: 10,
-    yMargin: 5,
-    baseColor: URColors.refreshButton,
-    listener: () => shoppingScene.nextQuestionSet()
-  } );
-  refreshButton.touchArea = refreshButton.localBounds.dilatedXY( 5, 5 );
-
-  // AccordionBox content
-  const contentNode = new VBox( {
-    spacing: 0, // no space here, we want refreshButton snug against bottom question
-    align: 'left',
-    children: [
-      new VBox( {
-        spacing: options.vBoxSpacing,
-        align: 'right',
-        children: [
-          unitRateQuestionNode,
-          questionsParent
-        ]
+      // AccordionBox options
+      // tight vertical margins and spacing, see https://github.com/phetsims/unit-rates/issues/140
+      titleYMargin: 0,
+      contentXMargin: 10,
+      contentYMargin: 6,
+      contentYSpacing: 2,
+      titleNode: new Text( questionsString, {
+        font: URConstants.ACCORDION_BOX_TITLE_FONT,
+        maxWidth: 100
       } ),
-      refreshButton
-    ]
-  } );
 
-  AccordionBox.call( this, contentNode, options );
+      // VBox options
+      vBoxSpacing: 12 // vertical spacing between UI elements in the accordion box's content
 
-  // Adjust rectangle to match accordion box size.
-  thisBoundsNode.setRectBounds( this.localBounds );
-  this.addChild( thisBoundsNode );
-  thisBoundsNode.moveToBack();
+    }, options );
 
-  // @private cleanup that's specific to this Node
-  this.disposeShoppingQuestionsAccordionBox = () => {
-    shoppingScene.questionSetProperty.unlink( questionSetObserver );
-    unitRateQuestionNode.dispose();
-    questionsParent.getChildren().forEach( child => {
-      assert && assert( child instanceof ShoppingQuestionNode );
-      child.dispose();
+    // An invisible rectangle that has the same bounds as the accordion box. Used to position the keypad.
+    // Dimensions will be set after calling super.  This was added so when converting to an ES6 class, because
+    // we can't use this before super.  See https://github.com/phetsims/tasks/issues/1026#issuecomment-594357784
+    const thisBoundsNode = new Rectangle( 0, 0, 1, 1, {
+      visible: false,
+      pickable: false
     } );
-    refreshButton.dispose(); // workaround for memory leak https://github.com/phetsims/unit-rates/issues/207
-  };
-}
 
-unitRates.register( 'ShoppingQuestionsAccordionBox', ShoppingQuestionsAccordionBox );
+    // 'Unit Rate?' question, dispose required.
+    // This question is separate because it does not change when the refresh button is pressed.
+    const unitRateQuestionNode = new ShoppingQuestionNode( shoppingScene.unitRateQuestion, thisBoundsNode, keypadLayer, {
+      denominatorVisible: true
+    } );
 
-export default inherit( AccordionBox, ShoppingQuestionsAccordionBox, {
+    // Below the 'Unit Rate?' question is a set of questions that change when the refresh button is pressed.
+    const questionsParent = new VBox( {
+      align: 'right',
+      spacing: options.vBoxSpacing
+    } );
+    const questionSetObserver = questionSet => {
+
+      // remove previous questions
+      questionsParent.getChildren().forEach( child => {
+        assert && assert( child instanceof ShoppingQuestionNode );
+        child.dispose();
+      } );
+      questionsParent.removeAllChildren();
+
+      // add new questions, dispose required
+      const questionNodes = [];
+      for ( let i = 0; i < questionSet.length; i++ ) {
+        questionNodes.push( new ShoppingQuestionNode( questionSet[ i ], thisBoundsNode, keypadLayer ) );
+      }
+      questionsParent.setChildren( questionNodes );
+    };
+    shoppingScene.questionSetProperty.link( questionSetObserver ); // unlink in dispose
+
+    // Refresh button, advances to the next question set
+    const refreshButton = new RefreshButton( {
+      iconScale: 0.36,
+      xMargin: 10,
+      yMargin: 5,
+      baseColor: URColors.refreshButton,
+      listener: () => shoppingScene.nextQuestionSet()
+    } );
+    refreshButton.touchArea = refreshButton.localBounds.dilatedXY( 5, 5 );
+
+    // AccordionBox content
+    const contentNode = new VBox( {
+      spacing: 0, // no space here, we want refreshButton snug against bottom question
+      align: 'left',
+      children: [
+        new VBox( {
+          spacing: options.vBoxSpacing,
+          align: 'right',
+          children: [
+            unitRateQuestionNode,
+            questionsParent
+          ]
+        } ),
+        refreshButton
+      ]
+    } );
+
+    super( contentNode, options );
+
+    // Adjust rectangle to match accordion box size.
+    thisBoundsNode.setRectBounds( this.localBounds );
+    this.addChild( thisBoundsNode );
+    thisBoundsNode.moveToBack();
+
+    // @private cleanup that's specific to this Node
+    this.disposeShoppingQuestionsAccordionBox = () => {
+      shoppingScene.questionSetProperty.unlink( questionSetObserver );
+      unitRateQuestionNode.dispose();
+      questionsParent.getChildren().forEach( child => {
+        assert && assert( child instanceof ShoppingQuestionNode );
+        child.dispose();
+      } );
+      refreshButton.dispose(); // workaround for memory leak https://github.com/phetsims/unit-rates/issues/207
+    };
+  }
 
   /**
    * @public
    * @override
    */
-  dispose: function() {
+  dispose() {
     this.disposeShoppingQuestionsAccordionBox();
-    AccordionBox.prototype.dispose.call( this );
+    super.dispose();
   }
-} );
+}
+
+unitRates.register( 'ShoppingQuestionsAccordionBox', ShoppingQuestionsAccordionBox );
+
+export default ShoppingQuestionsAccordionBox;
