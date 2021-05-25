00c0a3fd4de0ee7905db370a76a613e07750d79a
phet-dev <phetadmin@gmail.com>
2021-05-25 16:10:24 -0600

    update copyright dates from daily grunt work

--- js/shopping/model/ShoppingItemData.js
+++ js/shopping/model/ShoppingItemData.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * Data structures that describe item types in the 'Shopping' screen.
--- js/shopping/model/ShoppingModel.js
+++ js/shopping/model/ShoppingModel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * Model for the 'Shopping' screen. Also used as the base type for the 'Shopping Lab' screen.
