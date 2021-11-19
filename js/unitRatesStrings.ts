2cc5ee902149298f124e09ec5bcc889ccf1d01d9
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:06 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/unitRatesStrings.ts
@@ -0,0 +1,69 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import unitRates from './unitRates.js';
+
+type StringsType = {
+  'apple': string,
+  'apples': string,
+  'blueCandy': string,
+  'carrot': string,
+  'carrots': string,
+  'cost': string,
+  'cucumber': string,
+  'cucumbers': string,
+  'dollars': string,
+  'dollarSign': string,
+  'doubleNumberLine': string,
+  'doubleNumberLine1': string,
+  'doubleNumberLine2': string,
+  'enter': string,
+  'greenCandy': string,
+  'hours': string,
+  'lbs': string,
+  'lemon': string,
+  'lemons': string,
+  'miles': string,
+  'orange': string,
+  'oranges': string,
+  'pattern_0cost': string,
+  'pattern_costOf_0quantity_1units': string,
+  'pattern_0items_1cost': string,
+  'pattern_0value_1units': string,
+  'pear': string,
+  'pears': string,
+  'potato': string,
+  'potatoes': string,
+  'pound': string,
+  'pounds': string,
+  'poundsCapitalized': string,
+  'purpleCandy': string,
+  'questions': string,
+  'rate': string,
+  'rate1': string,
+  'rate2': string,
+  'redCandy': string,
+  'screen': {
+    'racingLab': string,
+    'shopping': string,
+    'shoppingLab': string
+  },
+  'timer1': string,
+  'timer2': string,
+  'tomato': string,
+  'tomatoes': string,
+  'unitRateQuestion': string,
+  'unit-rates': {
+    'title': string
+  }
+};
+
+const unitRatesStrings = getStringModule( 'UNIT_RATES' ) as StringsType;
+
+unitRates.register( 'unitRatesStrings', unitRatesStrings );
+
+export default unitRatesStrings;
