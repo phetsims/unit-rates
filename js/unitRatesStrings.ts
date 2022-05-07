a8c603d0e0d3b61e9492ccfd1da989cf95cd9ea7
Sam Reid <reids@colorado.edu>
2022-05-07 09:36:19 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/unitRatesStrings.ts
+++ js/unitRatesStrings.ts
@@ -8,57 +8,57 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import unitRates from './unitRates.js';
 
 type StringsType = {
-  'apple': string,
-  'apples': string,
-  'blueCandy': string,
-  'carrot': string,
-  'carrots': string,
-  'cost': string,
-  'cucumber': string,
-  'cucumbers': string,
-  'dollars': string,
-  'dollarSign': string,
-  'doubleNumberLine': string,
-  'doubleNumberLine1': string,
-  'doubleNumberLine2': string,
-  'enter': string,
-  'greenCandy': string,
-  'hours': string,
-  'lbs': string,
-  'lemon': string,
-  'lemons': string,
-  'miles': string,
-  'orange': string,
-  'oranges': string,
-  'pattern_0cost': string,
-  'pattern_costOf_0quantity_1units': string,
-  'pattern_0items_1cost': string,
-  'pattern_0value_1units': string,
-  'pear': string,
-  'pears': string,
-  'potato': string,
-  'potatoes': string,
-  'pound': string,
-  'pounds': string,
-  'poundsCapitalized': string,
-  'purpleCandy': string,
-  'questions': string,
-  'rate': string,
-  'rate1': string,
-  'rate2': string,
-  'redCandy': string,
+  'apple': string;
+  'apples': string;
+  'blueCandy': string;
+  'carrot': string;
+  'carrots': string;
+  'cost': string;
+  'cucumber': string;
+  'cucumbers': string;
+  'dollars': string;
+  'dollarSign': string;
+  'doubleNumberLine': string;
+  'doubleNumberLine1': string;
+  'doubleNumberLine2': string;
+  'enter': string;
+  'greenCandy': string;
+  'hours': string;
+  'lbs': string;
+  'lemon': string;
+  'lemons': string;
+  'miles': string;
+  'orange': string;
+  'oranges': string;
+  'pattern_0cost': string;
+  'pattern_costOf_0quantity_1units': string;
+  'pattern_0items_1cost': string;
+  'pattern_0value_1units': string;
+  'pear': string;
+  'pears': string;
+  'potato': string;
+  'potatoes': string;
+  'pound': string;
+  'pounds': string;
+  'poundsCapitalized': string;
+  'purpleCandy': string;
+  'questions': string;
+  'rate': string;
+  'rate1': string;
+  'rate2': string;
+  'redCandy': string;
   'screen': {
-    'racingLab': string,
-    'shopping': string,
-    'shoppingLab': string
-  },
-  'timer1': string,
-  'timer2': string,
-  'tomato': string,
-  'tomatoes': string,
-  'unitRateQuestion': string,
+    'racingLab': string;
+    'shopping': string;
+    'shoppingLab': string;
+  };
+  'timer1': string;
+  'timer2': string;
+  'tomato': string;
+  'tomatoes': string;
+  'unitRateQuestion': string;
   'unit-rates': {
-    'title': string
+    'title': string;
   }
 };
 
