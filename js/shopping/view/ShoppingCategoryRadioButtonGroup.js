ea33468033cfd0491f16a5dc563283022e4b6da8
phet-dev <phetadmin@gmail.com>
2023-01-14 05:01:21 -0700

    update copyright dates from daily grunt work

--- js/shopping/view/ShoppingCategoryRadioButtonGroup.js
+++ js/shopping/view/ShoppingCategoryRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, University of Colorado Boulder
+// Copyright 2016-2023, University of Colorado Boulder
 
 /**
  * Radio button group for selecting a category of items in the 'Shopping' screen.
