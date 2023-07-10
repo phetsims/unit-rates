e7da4e253aa07941d5b61b1f07421bc52a6b3f15
pixelzoom <cmalley@pixelzoom.com>
2023-07-10 17:35:36 -0600

    convert to TypeScript

--- js/shopping/model/Bag.ts
+++ js/shopping/model/Bag.ts
@@ -8,47 +8,52 @@
  */
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
-import merge from '../../../../phet-core/js/merge.js';
-import URMovable from '../../common/model/URMovable.js';
+import URMovable, { URMovableOptions } from '../../common/model/URMovable.js';
 import unitRates from '../../unitRates.js';
+import ShoppingItem from './ShoppingItem.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+
+type SelfOptions = {
+  visible?: boolean; // is the bag initially visible?
+  items?: ShoppingItem[] | null; // items in the bag, null means the bag does not open when placed on the scale
+};
+
+type BagOptions = SelfOptions & URMovableOptions;
 
 export default class Bag extends URMovable {
 
+  public readonly name: string;
+  public readonly image: HTMLImageElement;
+  public readonly items: ShoppingItem[] | null;
+  public readonly visibleProperty: BooleanProperty;
+
   /**
-   * @param {string} name - for internal use
-   * @param {HTMLImageElement} image - image used by the view to represent this bag
-   * @param {Object} [options]
+   * @param name - for internal use
+   * @param image - image used by the view to represent this bag
+   * @param [providedOptions]
    */
-  constructor( name, image, options ) {
+  public constructor( name: string, image: HTMLImageElement, providedOptions?: BagOptions ) {
 
-    options = merge( {
+    const options = optionize<BagOptions, SelfOptions, URMovableOptions>()( {
 
-      visible: true, // {boolean} is the bag initially visible?
-
-      // {ShoppingItem[]|null} items in the bag, null means the bag does not open when placed on the scale
+      // SelfOptions
+      visible: true,
       items: null,
 
-      // URMovable options
-      animationSpeed: 400 // distance/second
-
-    }, options );
+      // URMovableOptions
+      animationSpeed: 400
+    }, providedOptions );
 
     super( options );
 
-    // @public (read-only)
     this.name = name;
     this.image = image;
     this.items = options.items;
 
-    // @public
     this.visibleProperty = new BooleanProperty( options.visible );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  reset() {
+  public override reset(): void {
     this.visibleProperty.reset();
     super.reset();
   }
