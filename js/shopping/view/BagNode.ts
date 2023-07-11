c7852c45b9c5fc42afd9be334e013547ceef4729
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 16:44:14 -0600

    convert to TypeScript

--- js/shopping/view/BagNode.ts
+++ js/shopping/view/BagNode.ts
@@ -6,21 +6,20 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import { Image } from '../../../../scenery/js/imports.js';
+import { Image, Node } from '../../../../scenery/js/imports.js';
 import URConstants from '../../common/URConstants.js';
 import unitRates from '../../unitRates.js';
 import BagDragListener from './BagDragListener.js';
+import Bag from '../model/Bag.js';
+import Shelf from '../model/Shelf.js';
+import Scale from '../model/Scale.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 
 export default class BagNode extends Image {
 
-  /**
-   * @param {Bag} bag
-   * @param {Shelf} shelf
-   * @param {Scale} scale
-   * @param {Node} bagLayer
-   * @param {Node} dragLayer
-   */
-  constructor( bag, shelf, scale, bagLayer, dragLayer ) {
+  private readonly disposeBagNode: () => void;
+
+  public constructor( bag: Bag, shelf: Shelf, scale: Scale, bagLayer: Node, dragLayer: Node ) {
 
     // This type does not propagate options to the supertype because the model determines position.
     super( bag.image, {
@@ -29,13 +28,13 @@ export default class BagNode extends Image {
     } );
 
     // origin is at bottom center
-    const positionObserver = position => {
+    const positionObserver = ( position: Vector2 ) => {
       this.centerX = position.x;
       this.bottom = position.y;
     };
     bag.positionProperty.link( positionObserver ); // unlink in dispose
 
-    const visibleObserver = visible => {
+    const visibleObserver = ( visible: boolean ) => {
       this.visible = visible;
     };
     bag.visibleProperty.link( visibleObserver ); // unlink in dispose
@@ -43,7 +42,6 @@ export default class BagNode extends Image {
     const dragListener = new BagDragListener( this, bag, shelf, scale, bagLayer, dragLayer );
     this.addInputListener( dragListener ); // removeInputListener in dispose
 
-    // @private
     this.disposeBagNode = () => {
       bag.positionProperty.unlink( positionObserver );
       bag.visibleProperty.unlink( visibleObserver );
@@ -51,11 +49,7 @@ export default class BagNode extends Image {
     };
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposeBagNode();
     super.dispose();
   }
