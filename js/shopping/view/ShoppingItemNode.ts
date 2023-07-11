8f99aa558b36eec3c6e9634096f6ce02140c28bd
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 17:40:46 -0600

    convert to TypeScript

--- js/shopping/view/ShoppingItemNode.ts
+++ js/shopping/view/ShoppingItemNode.ts
@@ -6,22 +6,21 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import { Image } from '../../../../scenery/js/imports.js';
+import { Image, Node } from '../../../../scenery/js/imports.js';
 import URConstants from '../../common/URConstants.js';
 import unitRates from '../../unitRates.js';
 import ShoppingItemDragListener from './ShoppingItemDragListener.js';
+import Shelf from '../model/Shelf.js';
+import Scale from '../model/Scale.js';
+import ShoppingItem from '../model/ShoppingItem.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 
 export default class ShoppingItemNode extends Image {
 
-  /**
-   * @param {ShoppingItem} item
-   * @param {Shelf} shelf
-   * @param {Scale} scale
-   * @param {scenery.Node} frontItemLayer
-   * @param {scenery.Node} backItemLayer
-   * @param {scenery.Node} dragLayer
-   */
-  constructor( item, shelf, scale, frontItemLayer, backItemLayer, dragLayer ) {
+  private readonly disposeShoppingItemNode: () => void;
+
+  public constructor( item: ShoppingItem, shelf: Shelf, scale: Scale,
+                      frontItemLayer: Node, backItemLayer: Node, dragLayer: Node ) {
 
     // This type does not propagate options to the supertype because the model determines position.
     super( item.image, {
@@ -30,16 +29,17 @@ export default class ShoppingItemNode extends Image {
     } );
 
     // origin is at bottom center
-    const positionObserver = position => {
+    const positionObserver = ( position: Vector2 ) => {
       this.centerX = position.x;
       this.bottom = position.y;
     };
     item.positionProperty.link( positionObserver ); // unlink in dispose
 
-    const visibleObserver = visible => {
+    const visibleObserver = ( visible: boolean ) => {
       this.visible = visible;
       if ( visible ) {
-        this.getParent() && this.getParent().removeChild( this );
+        const parent = this.parent;
+        parent && parent.removeChild( this );
 
         // put the Node in the proper layer
         if ( shelf.isItemInFrontRow( item ) || scale.isItemInFrontRow( item ) ) {
@@ -55,7 +55,6 @@ export default class ShoppingItemNode extends Image {
     const dragListener = new ShoppingItemDragListener( this, item, shelf, scale, frontItemLayer, backItemLayer, dragLayer );
     this.addInputListener( dragListener ); // removeInputListener in dispose
 
-    // @private
     this.disposeShoppingItemNode = () => {
       item.positionProperty.unlink( positionObserver );
       item.visibleProperty.unlink( visibleObserver );
@@ -63,11 +62,7 @@ export default class ShoppingItemNode extends Image {
     };
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposeShoppingItemNode();
     super.dispose();
   }
