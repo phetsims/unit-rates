7dbb71f448d9c1162345656dc001017af35382aa
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 12:45:08 -0600

    convert to TypeScript

--- js/common/view/ValueNode.ts
+++ js/common/view/ValueNode.ts
@@ -7,33 +7,42 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { Text } from '../../../../scenery/js/imports.js';
+import { Text, TextOptions } from '../../../../scenery/js/imports.js';
 import unitRates from '../../unitRates.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickOptional from '../../../../phet-core/js/types/PickOptional.js';
+
+type SelfOptions = {
+  valueToString?: ( value: number ) => string;
+};
+
+type ValueNodeOptions = SelfOptions & PickOptional<TextOptions, 'font'>;
 
 export default class ValueNode extends Text {
 
-  /**
-   * @param {Property.<number>} valueProperty
-   * @param {Object} [options]
-   */
-  constructor( valueProperty, options ) {
+  private readonly disposeValueNode: () => void;
 
-    options = merge( {
-      font: new PhetFont( 20 ),
-      valueToString: value => ( `${value}` )
-    }, options );
+  public constructor( valueProperty: TReadOnlyProperty<number>, providedOptions?: ValueNodeOptions ) {
+
+    const options = optionize<ValueNodeOptions, SelfOptions, TextOptions>()( {
+
+      // SelfOptions
+      valueToString: ( value: number ) => `${value}`,
+
+      // TextOptions
+      font: new PhetFont( 20 )
+    }, providedOptions );
 
     super( '' ); // string will be filled in by valueObserver
 
     // update value display
-    const valueObserver = value => {
+    const valueObserver = ( value: number ) => {
       this.string = options.valueToString( value );
     };
     valueProperty.link( valueObserver ); // unlink in dispose
 
-    // @private
     this.disposeValueNode = () => {
       valueProperty.unlink( valueObserver );
     };
@@ -41,11 +50,7 @@ export default class ValueNode extends Text {
     this.mutate( options );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     this.disposeValueNode();
     super.dispose();
   }
