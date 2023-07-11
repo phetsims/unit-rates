86af87f598cf5e7e59717815c6c60dc981cd8ee8
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 09:47:53 -0600

    convert to TypeScript

--- js/shopping/model/ShoppingQuestion.ts
+++ js/shopping/model/ShoppingQuestion.ts
@@ -8,32 +8,42 @@
 
 import Emitter from '../../../../axon/js/Emitter.js';
 import Property from '../../../../axon/js/Property.js';
-import merge from '../../../../phet-core/js/merge.js';
 import SunConstants from '../../../../sun/js/SunConstants.js';
 import unitRates from '../../unitRates.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+
+type AnswerOptions = {
+  valueFormat?: string; // format used by StringUtils.format to format the guess
+  maxDigits?: number; // maximum number of digits that can be entered on the keypad
+  maxDecimals?: number; // maximum number of decimal places that can be entered on the keypad
+  trimZeros?: boolean; // whether to trim trailing zeros in the decimal places
+};
 
 export default class ShoppingQuestion {
 
+  public readonly questionString: string;
+  public readonly answer: number;
+  public readonly numerator: number;
+  public readonly denominator: number;
+  public readonly numeratorString: string;
+  public readonly denominatorString: string;
+  public readonly answerOptions: AnswerOptions;
+
+  public readonly guessProperty: Property<number | null>; // the user's guess, null indicates no guess
+  public readonly correctEmitter: Emitter<[ShoppingQuestion]>; // emit is called when the question is answered correctly
+
   /**
-   * @param {string} questionString - the question string to be displayed
-   * @param {number} answer - the correct answer
-   * @param {number} numerator
-   * @param {number} denominator
-   * @param {string} numeratorString - the numerator to display when the answer is revealed
-   * @param {string} denominatorString - the denominator to display when the answer is revealed
-   * @param {Object} [answerOptions] - formatting for the answer (and guesses)
+   * @param questionString - the question string to be displayed
+   * @param answer - the correct answer
+   * @param numerator
+   * @param denominator
+   * @param numeratorString - the numerator to display when the answer is revealed
+   * @param denominatorString - the denominator to display when the answer is revealed
+   * @param [answerOptions] - formatting for the answer (and guesses)
    */
-  constructor( questionString, answer, numerator, denominator, numeratorString, denominatorString, answerOptions ) {
+  public constructor( questionString: string, answer: number, numerator: number, denominator: number,
+                      numeratorString: string, denominatorString: string, answerOptions?: AnswerOptions ) {
 
-    // @public (read-only)
-    this.answerOptions = merge( {
-      valueFormat: SunConstants.VALUE_NUMBERED_PLACEHOLDER, // {string} format used by StringUtils.format to format the guess
-      maxDigits: 4, // {number} maximum number of digits that can be entered on the keypad
-      maxDecimals: 2, // {number} maximum number of decimal places that can be entered on the keypad
-      trimZeros: false // {boolean} whether to trim trailing zeros in the decimal places
-    }, answerOptions );
-
-    // @public (read-only)
     this.questionString = questionString;
     this.answer = answer;
     this.numerator = numerator;
@@ -41,10 +51,15 @@ export default class ShoppingQuestion {
     this.numeratorString = numeratorString;
     this.denominatorString = denominatorString;
 
-    // @public {Property.<number|null>, the user's guess, null indicates no guess
-    this.guessProperty = new Property( null );
+    this.answerOptions = combineOptions<AnswerOptions>( {
+      valueFormat: SunConstants.VALUE_NUMBERED_PLACEHOLDER, // {string} format used by StringUtils.format to format the guess
+      maxDigits: 4, // {number} maximum number of digits that can be entered on the keypad
+      maxDecimals: 2, // {number} maximum number of decimal places that can be entered on the keypad
+      trimZeros: false // {boolean} whether to trim trailing zeros in the decimal places
+    }, answerOptions );
 
-    // @public emit is called when the question is answered correctly
+    this.guessProperty = new Property<number | null>( null );
+
     this.correctEmitter = new Emitter( {
       parameters: [ { valueType: ShoppingQuestion } ]
     } );
@@ -57,8 +72,7 @@ export default class ShoppingQuestion {
     } );
   }
 
-  // @public
-  reset() {
+  public reset(): void {
     this.guessProperty.reset();
   }
 }
