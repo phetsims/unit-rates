f24678822262633b7c17694277a5ab5b026ccb1c
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 11:51:52 -0600

    convert to TypeScript

--- js/racinglab/view/ResetRaceButton.ts
+++ js/racinglab/view/ResetRaceButton.ts
@@ -6,23 +6,27 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import { Image } from '../../../../scenery/js/imports.js';
-import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
+import { Image, NodeTranslationOptions } from '../../../../scenery/js/imports.js';
+import RectangularPushButton, { RectangularPushButtonOptions } from '../../../../sun/js/buttons/RectangularPushButton.js';
 import resetRaceButton_png from '../../../images/resetRaceButton_png.js';
 import unitRates from '../../unitRates.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+
+type SelfOptions = EmptySelfOptions;
+
+type ResetRaceButtonOptions = SelfOptions & NodeTranslationOptions & PickRequired<RectangularPushButtonOptions, 'listener'>;
 
 export default class ResetRaceButton extends RectangularPushButton {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
-    super( merge( {
+  public constructor( providedOptions: ResetRaceButtonOptions ) {
+    super( optionize<ResetRaceButtonOptions, SelfOptions, RectangularPushButtonOptions>()( {
+
+      // RectangularPushButtonOptions
       content: new Image( resetRaceButton_png, { scale: 0.5 } ),
       xMargin: 12,
       yMargin: 8
-    }, options ) );
+    }, providedOptions ) );
   }
 }
 
