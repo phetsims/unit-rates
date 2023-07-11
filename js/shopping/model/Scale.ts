5ae2455ec4e8dec8e62e454c50e2d41702c30b5a
pixelzoom <cmalley@pixelzoom.com>
2023-07-11 09:41:12 -0600

    convert to TypeScript

--- js/shopping/model/Scale.ts
+++ js/shopping/model/Scale.ts
@@ -9,54 +9,61 @@
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import merge from '../../../../phet-core/js/merge.js';
 import unitRates from '../../unitRates.js';
-import ShoppingContainer from './ShoppingContainer.js';
+import ShoppingContainer, { ShoppingContainerOptions } from './ShoppingContainer.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+
+type SelfOptions = {
+  quantityUnits: string; // units for quantity
+  quantityPerBag?: number; // quantity in each bag
+};
+
+type ScaleOptions = SelfOptions & ShoppingContainerOptions;
 
 export default class Scale extends ShoppingContainer {
 
-  /**
-   * @param {Property.<number>} unitRateProperty
-   * @param {Object} [options]
-   */
-  constructor( unitRateProperty, options ) {
+  public readonly quantityUnits: string;
 
-    options = merge( {
+  // description of pseudo-3D shape
+  public readonly width = 350; // diameter of the top platter
+  public readonly height = 60; // height of the front face
+  public readonly depth = 45; // depth, after flattening to 2D
+  public readonly perspectiveXOffset = 30; // offset for parallel perspective, after flattening to 2D
 
-      position: new Vector2( 0, 0 ), // {Vector2} position of the center of the scale's top surface
-      quantityUnits: '', // {string} units for quantity
+  public readonly yAboveScale: number; // any y value less than this is considered "above the scale"
+
+  public quantityUpdateEnabled: boolean;
+  public readonly quantityProperty: TReadOnlyProperty<number>;
+  public readonly costProperty: TReadOnlyProperty<number>;
+
+  public constructor( unitRateProperty: TReadOnlyProperty<number>, providedOptions: ScaleOptions ) {
+
+    const options = optionize<ScaleOptions, SelfOptions, ShoppingContainerOptions>()( {
+
+      // SelfOptions
+      quantityPerBag: 5, // quantity in each bag
 
       // ShoppingContainer options
-      numberOfBags: 4, // {number} maximum number of bags on the scale
-      bagSize: new Dimension2( 100, 100 ), // {number} dimensions of each bag
-      quantityPerBag: 5, // {number} quantity in each bag
-      bagRowYOffset: 5, // {number} offset of bag row from scale origin
-      numberOfItems: 15, // {number} maximum number of items on the shelf
-      itemSize: new Dimension2( 25, 25 ), // {number} dimensions of each item
-      backRowYOffset: -4, // // {number} offset of items back row from scale origin
-      frontRowYOffset: 12 // // {number} offset of items front row from scale origin
-
-    }, options );
+      position: new Vector2( 0, 0 ), // position of the center of the scale's top surface
+      numberOfBags: 4, // maximum number of bags on the scale
+      bagSize: new Dimension2( 100, 100 ), // dimensions of each bag
+      bagRowYOffset: 5, // offset of bag row from scale origin
+      numberOfItems: 15, // maximum number of items on the shelf
+      itemSize: new Dimension2( 25, 25 ), // dimensions of each item
+      backRowYOffset: -4, // offset of items back row from scale origin
+      frontRowYOffset: 12 // offset of items front row from scale origin
+    }, providedOptions );
 
     super( options );
 
-    // @public (read-only)
     this.quantityUnits = options.quantityUnits;
 
-    // @public (read-only) description of pseudo-3D shape
-    this.width = 350; // {number} diameter of the top platter
-    this.height = 60; // {number} height of the front face
-    this.depth = 45; // {number} depth, after flattening to 2D
-    this.perspectiveXOffset = 30; // {number} offset for parallel perspective, after flattening to 2D
-
-    // @public (read-only) any y value less than this is considered "above the scale"
     // Offset determined empirically, see https://github.com/phetsims/unit-rates/issues/174
     this.yAboveScale = this.position.y + 70;
 
-    // @public
     this.quantityUpdateEnabled = true;
 
-    // @public dispose not required, exists for sim lifetime
     this.quantityProperty = new DerivedProperty(
       [ this.numberOfBagsProperty, this.numberOfItemsProperty ],
       ( numberOfBags, numberOfItems ) => {
@@ -68,7 +75,6 @@ export default class Scale extends ShoppingContainer {
         }
       } );
 
-    // @public dispose not required, exists for sim lifetime
     this.costProperty = new DerivedProperty(
       [ this.quantityProperty, unitRateProperty ],
       ( quantity, unitRate ) => quantity * unitRate
