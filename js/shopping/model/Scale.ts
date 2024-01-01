94a9758e60695b8239e36ad269a5fc1d5dca28ed
Sam Reid <reids@colorado.edu>
2024-01-01 10:03:25 -0700

    Change accessNonDependencies: true => strictAxonDependencies: false, add query parameter, add package.json flag, add phetmarks radio buttons, see https://github.com/phetsims/axon/issues/441

--- js/shopping/model/Scale.ts
+++ js/shopping/model/Scale.ts
@@ -74,7 +74,7 @@ export default class Scale extends ShoppingContainer {
           return this.quantityProperty.value;
         }
       }, {
-        accessNonDependencies: true // see https://github.com/phetsims/unit-rates/issues/223#issuecomment-1861776252
+        strictAxonDependencies: false // see https://github.com/phetsims/unit-rates/issues/223#issuecomment-1861776252
       } );
 
     this.costProperty = new DerivedProperty(
