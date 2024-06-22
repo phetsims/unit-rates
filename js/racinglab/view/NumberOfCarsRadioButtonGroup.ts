b18812f57ac247d022f3736d50d8a56272fef2bc
phet-dev <phetadmin@gmail.com>
2024-06-22 03:23:32 -0600

    update copyright dates from daily grunt work

--- js/racinglab/view/NumberOfCarsRadioButtonGroup.ts
+++ js/racinglab/view/NumberOfCarsRadioButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /**
  * NumberOfCarsRadioButtonGroup switches between 1 and 2 cars in the 'Racing Lab' screen.
