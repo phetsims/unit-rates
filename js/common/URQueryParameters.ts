353cbc1efc7551763e2c89cf0c6013ba5d1c927d
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:16 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/URQueryParameters.ts
+++ js/common/URQueryParameters.ts
@@ -7,6 +7,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import unitRates from '../unitRates.js';
 
 const URQueryParameters = QueryStringMachine.getAll( {
